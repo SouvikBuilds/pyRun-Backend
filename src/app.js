b4841270e07b34d4deb21c12e2d9bf3e589ccc67
Souvik Chatterjee <souvikchatterjee@Souviks-MacBook-Air.local>
2025-12-26 09:53:11 +0530

    origin changed

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { handleError } from "./middleware/error.middleware.js";
 const app = express();
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: process.env.ORIGIN || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     optionsSuccessStatus: 204,
